90b232efd9995de15febc761c92e8069e6e94c3c
Nicolai Weitkemper <kontakt@nicolaiweitkemper.de>
2022-09-14 13:22:47 +0200

    plot MC histograms from YAML data

---
+++ plots_code/plot_from_data.py
@@ -0,0 +1,40 @@
+from pathlib import Path
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+import yaml
+from matplotlib import rc_context
+
+from common import PLOTS_DIR, STYLES, console
+
+PLOTS = [
+    {
+        'name': "dataset:raw:histogram",
+        'style': 'full',
+    },
+    {
+        'name': "dataset_500k:discretized:histogram",
+        'style': 'full',
+    },
+]
+
+for i, plot in enumerate(PLOTS):
+    with open(f"data/{plot['name']}.yaml") as f:
+        config = yaml.load(f, Loader=yaml.FullLoader)
+
+    with rc_context(STYLES[plot['style']]), console.status(f"Plotting ({i+1}/{len(PLOTS)})…"):
+        plt.hist(
+            config['bin_edges'][:-1], bins=config['bin_edges'], weights=config['hist'],
+            edgecolor='black',
+            # linewidth=1.2,
+        )
+        plt.xscale('log')
+        plt.yscale('log')
+        plt.xlabel(r'\texttt{MCPrimary.energy} [\si{\giga\electronvolt}]')
+        plt.ylabel(r'Count')
+        plt.grid()
+        plt.gca().set_axisbelow(True)
+        # sns.despine()
+        plt.tight_layout()
+        plt.savefig(PLOTS_DIR / f"{plot['name']}_{plot['style']}.pdf")
+        plt.clf()
