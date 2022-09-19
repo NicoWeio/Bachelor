c15ab3cca9b61cab07578d1f0a224d1e675374dd
Nicolai Weitkemper <kontakt@nicolaiweitkemper.de>
2022-09-19 21:32:18 +0200

    [WIP] errorbars on spectra
    This is not as easy to get right with variable bin sizes…

--- plots_code/plot_spectra.py
+++ plots_code/plot_spectra.py
@@ -11,6 +11,8 @@ from common import STYLES, console
 LOWER_LIMIT = 10**2.1
 UPPER_LIMIT = 10**5
 
+PLOT_XLIM = (10**2, 10**5.5)
+
 # XTICKS = np.logspace(np.log10(lower_limit), np.log10(upper_limit), NUM_BINS+1)
 XTICKS = np.geomspace(10**2, 10**8, (8-2+1))
 
@@ -19,13 +21,32 @@ api = wandb.Api()
 
 run = api.run("nicoweio/dsea-corn/3s0n2bmo")
 BIN_EDGES = np.array(run.summary['bin_edges'])
-STYLE = 'halfwidth'
+STYLE = 'full'
+# %%
 
-pred_spectrum = np.array(run.summary['bootstrap/pred_spectra'][0])
-pred_spectrum = np.append(pred_spectrum, pred_spectrum[-1])
-true_spectrum = np.array(run.summary['bootstrap/true_spectra'][0])
-true_spectrum = np.append(true_spectrum, true_spectrum[-1])
+pred_spectra = np.vstack(run.summary['bootstrap/pred_spectra'])
+true_spectra = np.vstack(run.summary['bootstrap/true_spectra'])
 
+# repeat the last entry to match the number of bin EDGES
+pred_spectra = np.append(pred_spectra, pred_spectra[:, -1, np.newaxis], axis=1)
+true_spectra = np.append(true_spectra, true_spectra[:, -1, np.newaxis], axis=1)
+
+# %%
+# determine median and 68% confidence interval
+pred_spectrum = np.median(pred_spectra, axis=0)
+true_spectrum = np.median(true_spectra, axis=0)
+
+pred_spectrum_error = np.percentile(pred_spectra, [16, 84], axis=0)
+true_spectrum_error = np.percentile(true_spectra, [16, 84], axis=0)
+
+# %%
+rel_error = (pred_spectrum - true_spectrum) / true_spectrum
+
+pred_percentiles_as_err = np.array([
+    # matplotlib expects [lower, upper]
+    pred_spectrum - pred_spectrum_error[0],
+    pred_spectrum_error[1] - pred_spectrum,
+])
 # %%
 with rc_context(STYLES[STYLE]), console.status(f"Plotting spectrum…"):
     fig, axs = plt.subplots(
@@ -37,13 +58,39 @@ with rc_context(STYLES[STYLE]), console.status(f"Plotting spectrum…"):
     # plt.hist(BINS[:-1], BINS, weights=spectrum_from_labels(labels), color='red', label='true class')
 
     axs[0].step(BIN_EDGES, true_spectrum, where='post', drawstyle='steps-mid', color='C1', linewidth=2, label='true class')
+
     axs[0].step(BIN_EDGES, pred_spectrum, where='post', drawstyle='steps-mid', color='C0', zorder=10, label='predicted probas')
+
+
+    # get the middle points of the bin edges
+    # bin_centers = (BIN_EDGES[:-1] + BIN_EDGES[1:]) / 2
+    # NOTE: We need to account for the log scale
+    bin_centers = np.exp((np.log(BIN_EDGES[:-1]) + np.log(BIN_EDGES[1:])) / 2)
+
+    # manually set the last center, because we crop the plot…
+    # bin_centers[-1] = (BIN_EDGES[-2] + PLOT_XLIM[1]) / 2
+    bin_centers[-1] = np.exp((np.log(BIN_EDGES[-2]) + np.log(PLOT_XLIM[1])) / 2)
+
+    axs[0].errorbar(bin_centers,
+                    pred_spectrum[:-1],
+                    # yerr=pred_spectrum_error,
+                    yerr=pred_percentiles_as_err[:, :-1],
+                    drawstyle='steps-post',
+                    # color='blue',
+                    zorder=20,
+                    # label="predicted probas (bootstrap median, TODO%ile)"
+                    linewidth=0,
+                    elinewidth=1,
+                    ecolor='red',
+                    )
+
+
     axs[0].set_ylabel('probability')  # TODO
     axs[0].set_yscale('log')
     axs[0].legend()
     # axs[0].set_xlim(100, 10_000)
 
-    axs[1].step(BIN_EDGES, (pred_spectrum - true_spectrum) / true_spectrum, where='post', color='C2')
+    axs[1].step(BIN_EDGES, rel_error, where='post', color='C2')
     axs[1].set_ylabel('relative deviation')
     axs[1].set_xscale('log')
     axs[1].set_xlabel(r'energy \mathbin{/} \si{\giga\electronvolt}')
@@ -65,7 +112,7 @@ with rc_context(STYLES[STYLE]), console.status(f"Plotting spectrum…"):
         ax.grid()
         # Crop the x-axis so the overflow bin does not take up too much space;
         # suggestion by Karolin.
-        ax.set_xlim(10**2, 10**5.5)
+        ax.set_xlim(*PLOT_XLIM)
 
     # grey out under-/overflow
     # for ax in axs:
