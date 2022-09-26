fa266407b2e4a5e486d7b9c9c421276a1b800ae0
Nicolai Weitkemper <kontakt@nicolaiweitkemper.de>
2022-09-26 16:04:46 +0200

    [WIP] plot: bootstrap distributions

--- plots_code/plot_bootstrap_distributions.py
+++ plots_code/plot_bootstrap_distributions.py
@@ -1,11 +1,11 @@
 # %%
-import wandb
 import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
+import wandb
 from matplotlib import rc_context
 
-from common import STYLES, console
+from common import PLOTS_DIR, STYLES, console
 
 # %%
 LOWER_LIMIT = 10**2.1
@@ -21,23 +21,39 @@ api = wandb.Api()
 
 run = api.run("nicoweio/dsea-corn/24sfwi8p")
 BIN_EDGES = np.array(run.summary['bin_edges'])
-STYLE = 'full'
+STYLE = 'doubleheight'
 # %%
 pred_spectra = np.vstack(run.summary['bootstrap/pred_spectra'])
 true_spectra = np.vstack(run.summary['bootstrap/true_spectra'])
 
 # %%
-BIN_INDEX = 0
+with rc_context(STYLES[STYLE]), console.status(f"Plotting spectrum…"):
+    fig, axs = plt.subplots(
+        5, 2,
+        # sharex=True,
+        # sharey=True,
+    )
+    BIN_INDICES = list(range(10))
+    assert len(BIN_INDICES) == axs.size
 
-hist, edges = np.histogram(
-    pred_spectra[:, BIN_INDEX],
-    bins=10,
-    # range=(0, 1),
-    # density=True,
-)
-# %%
-# plot the histogram
-plt.bar(edges[:-1], hist, width=np.diff(edges), ec='k', align='edge')
-plt.xlabel("TODO")
-plt.ylabel("count")
-# %%
+    for ax, BIN_INDEX in zip(axs.flat, BIN_INDICES):
+        hist, edges = np.histogram(
+            pred_spectra[:, BIN_INDEX],
+            bins=10,
+            # range=(0, 1),
+            # density=True,
+        )
+        # plot the histogram
+        ax.bar(edges[:-1], hist, width=np.diff(edges), ec='k', align='edge')
+        ax.axvline(true_spectra[:, BIN_INDEX].mean(), color='C1', linestyle='--')
+        ax.set_xlabel("probability density")
+        ax.set_ylabel("count")
+        ax.set_title(f"Bin {BIN_INDEX+1}")
+
+    # fig.text(0.5, 0.04, "probability density", ha='center')
+    # fig.text(0.04, 0.5, "count", va='center', rotation='vertical')
+
+    plt.tight_layout()
+
+    # %%
+    plt.savefig(PLOTS_DIR / f"bootstrap:distributions_{STYLE}.pdf")
